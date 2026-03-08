a96c50bb772599015033988e3ddcb38e4c16affd
Mohd Saad <99261688+AnsariSaad83299@users.noreply.github.com>
2026-03-08 18:29:05 -0400

    Change API key to placeholder in inference script
    Updated API key placeholder in roboflow-inference.py.

--- Weapon-Detection-Backend/roboflow-inference.py
+++ Weapon-Detection-Backend/roboflow-inference.py
@@ -2,7 +2,7 @@ from inference_sdk import InferenceHTTPClient
 
 CLIENT = InferenceHTTPClient(
     api_url="https://detect.roboflow.com",
-    api_key="JLRUneIOGPL8doqgZfe7"
+    api_key="" #place ur api key
 )
 
 result = CLIENT.infer('test-image.png', model_id="wpn_pintol_di_kita/1")
