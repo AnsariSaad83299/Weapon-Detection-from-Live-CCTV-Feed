575f800eb61c5875f925d3a201c3bf27391e01e1
Mohd Saad <99261688+AnsariSaad83299@users.noreply.github.com>
2026-03-08 18:25:36 -0400

    Refactor initialization of InferencePipeline

--- Weapon-Detection-Backend/roboflow-inference-video.py
+++ Weapon-Detection-Backend/roboflow-inference-video.py
@@ -1,13 +1,10 @@
-# Import the InferencePipeline object
 from inference import InferencePipeline
-# Import the built in render_boxes sink for visualizing results
 from inference.core.interfaces.stream.sinks import render_boxes
 
-# initialize a pipeline object
 pipeline = InferencePipeline.init(
-    model_id="wpn_pintol_di_kita/1", # Roboflow model to use
-    video_reference='./test.mp4', # Path to video, device id (int, usually 0 for built in webcams), or RTSP stream url
-    on_prediction=render_boxes, # Function to run after each prediction
+    model_id="wpn_pintol_di_kita/1", 
+    video_reference='./test.mp4', 
+    on_prediction=render_boxes, 
 )
 pipeline.start()
 pipeline.join()
